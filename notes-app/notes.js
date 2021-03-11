a9cf6638bdb894c721071c4249d963f14cc7abcd
Dom Hallan <dom@polyglotdev.com>
2021-03-11 16:40:18 -0600

    🔧  add favicons and complete DOM challenge

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,1 +1,8 @@
-console.log('my notes')
+const p = document.querySelector('p')
+// p.remove()
+
+const ps = document.querySelectorAll('p')
+ps.forEach((p) => {
+  // p.textContent = 'keep it 1000.'
+  // p.remove()
+})
---
+++ todos-app/todos.js
@@ -0,0 +1,9 @@
+console.log('todos')
+
+const getAllParagraphs = document.querySelectorAll('p')
+getAllParagraphs.forEach((p) => {
+  // console.log(p.textContent)
+  // if (p.textContent === `Workout`) {
+  //   p.remove()
+  // }
+})
