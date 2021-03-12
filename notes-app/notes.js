29c50cb62d7527782636d38ccc46c1f6e7fef185
Dom Hallan <dom@polyglotdev.com>
2021-03-11 19:44:27 -0600

    🔧  complete the handle user interaction module challenge

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,8 +1,29 @@
-const p = document.querySelector('p')
-// p.remove()
+const notes = [
+  {
+    title: `This is JS`,
+    body: `Man isn't JavaScript grand?`
+  },
+  {
+    title: `Moving On`,
+    body: `Moving on is hard to do, but it is part of life.`
+  },
+  {
+    title: `Work smarter, not harder`,
+    body: `Employers ask more of you daily and in that we have to remember to work smarter and not harder.`
+  }
+]
 
-const ps = document.querySelectorAll('p')
-ps.forEach((p) => {
-  // p.textContent = 'keep it 1000.'
-  // p.remove()
-})
+const getTimesClicked = () => {
+  let clickedCount = 0
+  const getCreateNoteButton = document
+    .getElementById('create-note')
+    .addEventListener('click', (event) => {
+      if (event) {
+        clickedCount++
+        event.target.textContent = `You clicked this button ${clickedCount} times.`
+      }
+      return clickedCount
+    })
+}
+
+getTimesClicked()
--- todos-app/todos.js
+++ todos-app/todos.js
@@ -1,9 +1,36 @@
-console.log('todos')
+const todos = [
+  {
+    text: 'Get thru training',
+    completed: true
+  },
+  {
+    text: 'Call Mom',
+    completed: false
+  },
+  {
+    text: 'Walk the dog',
+    completed: false
+  }
+]
 
-const getAllParagraphs = document.querySelectorAll('p')
-getAllParagraphs.forEach((p) => {
-  // console.log(p.textContent)
-  // if (p.textContent === `Workout`) {
-  //   p.remove()
-  // }
+const notesSetup = (todos, headingValue, todoElement) => {
+  let arr = []
+  todos.forEach((todo) => {
+    arr.push(todo.text)
+  })
+  const createHeading = document.createElement(headingValue)
+  createHeading.textContent = `You have ${arr.length} todos remaining.`
+  document.body.appendChild(createHeading)
+
+  arr.forEach((item) => {
+    const todos = document.createElement(todoElement)
+    todos.textContent = item
+    document.body.appendChild(todos)
+  })
+}
+
+notesSetup(todos, 'h2', 'p')
+
+document.getElementById('create-notes').addEventListener('click', () => {
+  console.log('create notes button clicked')
 })
